a17aee7d7c652a13165d6ff608012828cb2415b6
SiMamank115 <faizfullafalisa@gmail.com>
2022-09-25 16:43:38 +0700

    barba implement (sik ngebug sebagian)

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import { HeroText } from "./heroText.js";
-import { scrollFunction , jqScroll } from "./utils/backToTop.js";
+import { scrollFunction, jqScroll } from "./utils/backToTop.js";
 import { smoothScroll as smScroll } from "./utils/smoothScroll.js";
 import { refreshScroll as rfScroll } from "./utils/refreshScroll.js";
 import { customElementEs5Adapter as particleAdapter } from "./utils/particles/custom-elements-es5-adapter.js";
@@ -26,21 +26,70 @@ let animGaris = heroText.animateLine();
 let animText = heroText.animateText();
 
 const animationToggleOff = () => {
-  window.clearInterval(animGaris);
-  window.clearInterval(animText);
+    window.clearInterval(animGaris);
+    window.clearInterval(animText);
 };
-document.querySelectorAll('.close').forEach(e => {
+document.querySelectorAll(".close").forEach((e) => {
     e.addEventListener("click", function () {
-        this.parentElement.classList.add('dissmised')
-      });
-})
+        this.parentElement.classList.add("dissmised");
+    });
+});
 
 //navbar and socialmedia animation
-$(document).ready(function () {
+let animateNavbar = () => {
     setTimeout(() => {
-        $("nav.navbar").toggleClass("hide")
+        $("nav.navbar").toggleClass("hide");
     }, 100);
     setTimeout(() => {
-        $("#socialmedia").toggleClass("dissmised")
+        $("#socialmedia").toggleClass("dissmised");
     }, 600);
-})
+};
+$(document).ready(animateNavbar);
+
+
+barba.init({
+    transitions: [
+        {
+            name: "slide-transition",
+            leave(data) {
+                gsap.to(data.current.container,{
+                    opacity:0
+                })
+                if(data.current.url == "/gallery.html") {
+                    gsap.to(".barba-transition", {
+                        duration:0,
+                        x: "100%",
+                    });
+                }
+                
+                return gsap.to(".barba-transition", {
+                    ease: "expo.out",
+                    duration:1,
+                    x: "0",
+                });
+            },
+            enter(data) {
+                gsap.to(data.next.container,{
+                    opacity:0
+                })
+                
+                return gsap.to(".barba-transition", {
+                    duration:1,
+                    ease: "expo.in",
+                    x: (data.current.url == "/gallery.html"?"-100%":"100%"),
+                    onComplete: () => {
+                        gsap.to(".barba-transition", {
+                            x:"-100%",
+                            duration:0
+                        })
+                        gsap.to(data.next.container,{
+                            opacity:1,
+                            duration:0.5
+                        })
+                        animateNavbar();
+                    },
+                });
+            },
+        },
+    ],
+});
