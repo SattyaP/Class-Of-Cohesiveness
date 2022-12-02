1b24e16e578091fdb2ad6a0c8ba622be004a64a6
SiMamank115 <faizfullafalisa@gmail.com>
2022-12-02 18:37:25 +0700

    Carousel lazy load and next/prev button fix

--- app/utils/slickInitialize.js
+++ app/utils/slickInitialize.js
@@ -1,46 +1,29 @@
 export function slickInitialize() {
     $(".slider-before").click(async (e) => {
         let el = $(e.currentTarget),
-        images = [
-            "img/gallery/a-1.webp",
-            "img/gallery/a-2.webp",
-            "img/gallery/a-3.webp",
-            "img/gallery/a-4.webp",
-            "img/gallery/a-5.webp",
-            "img/gallery/a-6.webp",
-            "img/gallery/a-7.webp",
-            "img/gallery/a-8.webp"
-        ], i = 0,slider = document.createElement("div");
-        slider.classList.add(..."row homeslider".split(' '))
+            images = $(e.currentTarget).data("image").split(";"),
+            i = 0,
+            slider = document.createElement("div");
+        slider.classList.add(..."row homeslider".split(" "));
         $(el.children().children()[0]).removeClass("d-none");
-        for(i;i<images.length;i++) {
+        console.log(images);
+        for (i; i < images.length; i++) {
             await $.ajax({
                 type: "get",
                 url: images[i],
                 cache: true,
-                processData : false,
-            }).always(e=> {
-                slider.innerHTML += `<img class="img-fluid img-slider" src="${images[i]}" alt="">`
+                processData: false,
+            }).always((e) => {
+                slider.innerHTML += `<img class="img-fluid img-slider" src="${images[i]}" alt="">`;
             });
         }
-        $(el).parent().append(slider);
-        $(el).hide()
-        $(slider).slick({
+        await $(el).parent().append(slider);
+        await $(slider).slick({
             autoplay: true,
             autoplaySpeed: 5000,
             infinite: true,
             adaptiveHeight: true,
         });
-    });
-    return $(document).ready(function () {
-        $(".homeslider").slick({
-            autoplay: true,
-            // lagi benerin anjir gerak mulu
-            // hooh ae dah 🗿
-            // idupin ah 🗿
-            autoplaySpeed: 5000,
-            infinite: true,
-            adaptiveHeight: true,
-        });
+        await $(el).remove();
     });
 }
